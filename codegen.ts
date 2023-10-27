9b4872408888d9f0ba97d578055f6d9672d6691d
Masaki Murano <mrnmsk.0range.m2a@gmail.com>
2023-10-27 18:30:22 +0900

    GraphQLのQueryをコード生成できた！

---
+++ codegen.ts
@@ -0,0 +1,22 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  schema: 'https://beta.pokeapi.co/graphql/v1beta',
+  // schema: 'MY_SCHEMA_PATH',
+  documents: './src/**/*.graphql',
+  generates: {
+    './src/app/api/generated.ts': {
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        {
+          'typescript-rtk-query': {
+            importBaseApiFrom: 'api',
+            exportHooks: true
+          }
+        }
+      ]
+    }
+  }
+}
+export default config
